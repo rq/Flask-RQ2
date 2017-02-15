250e66e41b7c531fa5b1bdc1d9811121d5bf4fbc
Jannis Leidel <jannis@leidel.info>
2017-02-15 11:26:27 +0100

    Run tests on Python 3.6.

--- setup.py
+++ setup.py
@@ -74,6 +74,7 @@ setup_params = dict(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'Intended Audience :: End Users/Desktop',
