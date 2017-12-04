f03b3991f40f31f6aa27b6ebcef2c2215d0a9e25
Jannis Leidel <jannis@leidel.info>
2017-12-04 22:01:04 +0100

    Dropped Python 3.4 support.

--- setup.py
+++ setup.py
@@ -72,7 +72,6 @@ setup_params = dict(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Environment :: Web Environment',
