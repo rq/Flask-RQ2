43b0f3953ae1046a2b169d180a513dec148a715f
Jannis Leidel <jannis@leidel.info>
2018-12-20 11:30:10 +0100

    Require redis-py >= 3.0 (via rq-scheduler >= 0.9.0 and rq >= 0.13.0). Refs #75.

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ with codecs.open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 tests_require = [
     'pytest',
-    'redis<3.0.0',
-    'rq-scheduler>=0.8.3,<0.9.0',
+    'redis>3.0.0',
+    'rq-scheduler>=0.9.0',
     'flask-cli>=0.4.0',
     'pytest>=2.8.0',
     'pytest-cache>=1.0',
@@ -60,9 +60,9 @@ setup(
     setup_requires=['setuptools_scm'] + pytest_runner,
     install_requires=[
         'Flask>=0.10',
-        'rq>=0.12.0,<0.13.0',
-        'redis<3.0.0',
-        'rq-scheduler>=0.8.3,<0.9.0',
+        'rq>=0.13.0',
+        'redis>=3.0.0',
+        'rq-scheduler>=0.9.0',
     ],
     extras_require=extras_require,
     tests_require=tests_require,
