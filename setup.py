e0d7b3e8f6408b84b4c4474df99750c5acddbd4b
Jannis Leidel <jannis@leidel.info>
2020-06-09 17:10:43 +0200

    Use simple requirement files.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 import codecs
 import os
-import sys
 
 from setuptools import find_packages, setup
 
@@ -9,32 +8,6 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
-
-tests_require = [
-    "redis>3.0.0",
-    "rq-scheduler>=0.9.0",
-    "flask-cli>=0.4.0",
-    "pytest<5.0.0 ; python_version < '3.0'",
-    "pytest>=5.0.0 ; python_version >= '3.0'",
-    "pytest-cache>=1.0",
-    "pytest-flake8>=0.5",
-    "pytest-wholenodeid",
-    "pytest-coverage",
-    "pytest-pythonpath",
-]
-
-extras_require = {
-    "docs": ["Sphinx>=1.4"],
-    "cli": ["Flask-CLI>=0.4.0"],
-    "tests": tests_require,
-}
-
-extras_require["all"] = []
-for reqs in extras_require.values():
-    extras_require["all"].extend(reqs)
-
-needs_pytest = set(["pytest", "test", "ptr"]).intersection(sys.argv)
-pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 setup(
     name="Flask-RQ2",
@@ -50,23 +23,24 @@ setup(
     zip_safe=False,
     include_package_data=True,
     platforms="any",
-    setup_requires=["setuptools_scm"] + pytest_runner,
+    setup_requires=["setuptools_scm"],
     install_requires=[
         "Flask>=0.10",
         "rq>=0.13.0",
         "redis>=3.0.0",
         "rq-scheduler>=0.9.0",
     ],
-    extras_require=extras_require,
-    tests_require=tests_require,
+    extras_require={
+        "cli": ["Flask-CLI>=0.4.0"],
+    },
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "Intended Audience :: End Users/Desktop",
